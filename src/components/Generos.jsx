310c33a504e685810d20f6439a27f131789e4226
mauriciocarpiohtml <carpio.html@gmail.com>
2023-02-21 10:00:22 -0300

    error clase tailwind solucionado

--- src/components/Generos.jsx
+++ src/components/Generos.jsx
@@ -8,12 +8,12 @@ function Generos() {
     const {setGenero} = useContext(SpotifyContext)
   return (
     
-       <div className='mx-auto '>
+       <div className='mx-auto'>
           <div className='flex flex-col md:flex-row md:justify-between gap-3 w-full'>
               <h3 className='font-bold text-white mx-auto uppercase  text-xl'>Descubrir</h3>
               <select
               onChange={(e) => setGenero(e.target.value)} 
-              className=' cursor-pointer  p-3 bg-black text-gray-300 text-base rounded-lg outiline-none  lg:w-[13%] sm:w-[50%] mx-auto '>
+              className='cursor-pointer p-3 bg-black text-gray-300 text-base rounded-lg outline-none lg:w-[13%] sm:w-[50%] mx-auto'>
                   {genres.map((genre=>
                       <option
                       key={genre.value}
