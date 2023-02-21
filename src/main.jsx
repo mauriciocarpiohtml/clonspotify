64061aa1f4b523028e4306275420a85402976a81
mauriciocarpiohtml <carpio.html@gmail.com>
2023-02-21 19:46:37 -0300

    main arreglado

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import Artistas from './pages/Artistas';
-import Tendencias from './pages/Tendencias';
-import { MyContext } from './context/MyContext';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import Artistas from './pages/Artistas'
+import Tendencias from './pages/Tendencias'
+import { MyContext } from './context/MyContext'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  {path:'/', element:<App/>},
+  {path:'/artistas/:id', element:<Artistas/>},
+  {path:'/tendencias', element:<Tendencias/>}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MyContext>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/artistas/:id" component={Artistas} />
-          <Route path="/tendencias" component={Tendencias} />
-        </Switch>
-      </Router>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
     </MyContext>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
